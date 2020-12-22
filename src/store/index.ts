92da6886dac9e9c06193affc485eec35be6b4f4d
Achim <achim.schneider@equeo.de>
2020-12-22 13:59:26 +0100

    cleanup & optimization

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
-import { combineReducers, createStore } from "redux";
 import { createSelectorHook, useDispatch as _useDispatch } from "react-redux";
+import { combineReducers, createStore } from "redux";
 import tasksReducer from "./tasks/tasksReducer";
 import uiReducer from "./ui/uiReducer";
 
